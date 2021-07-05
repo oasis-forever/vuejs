4cf0482ee774ed87afd459c7faa40d0e424b6b54
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-07-05 11:53:52 +0900

    Create Vue app

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      product: {
+        name: 'Great Socks',
+        description: 'Smell-proof socks',
+        price: '980 JPY for 3 pairs'
+      }
+    }
+  }
+})
