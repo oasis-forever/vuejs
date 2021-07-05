728ed76e4844c35e152cca22faf24518707385cd
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-07-05 12:29:39 +0900

    Bind attribute

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const app = Vue.createApp({
   data() {
     return {
+      github: {
+        url: 'https://github.com/oasis-forever/vuejs_tutorial'
+      },
       product: {
         name: 'Great Socks',
         description: 'Smell-proof socks',
-        price: '980 JPY for 3 pairs'
+        price: '980 JPY for 3 pairs',
+        image: './assets/images/socks_blue.jpg'
       }
     }
   }
