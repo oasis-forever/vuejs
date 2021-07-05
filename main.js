5fc5e7a70acfc5a7311054495b9cc776708d1c16
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-07-05 14:19:31 +0900

    Commit list rendering

--- main.js
+++ main.js
@@ -5,13 +5,19 @@ const app = Vue.createApp({
         url: 'https://github.com/oasis-forever/vuejs_tutorial'
       },
       product: {
+        onSale: true,
         name: 'Great Socks',
         description: 'Smell-proof socks',
+        ingredients: ['50% cotton', '30% wool', '20% polyester'],
+        sizes: ['S', 'M', 'L', 'XL'],
         price: '980 JPY for 3 pairs',
         image: './assets/images/socks_blue.jpg',
-        inventory: 0,
-        onSale: true
+        inventory: 100,
+        variants: [
+          { id: 2234, color: 'green' },
+          { id: 2235, color: 'blue' }
+        ]
       }
     }
   }
-})
+});
